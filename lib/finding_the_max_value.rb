32931633213a96611700f127839028fb967017e2
Arpita Dutta <duttaarpita22@gmail.com>
2020-07-21 18:36:04 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,6 @@
 def find_max_value(array)
   count = 0
-  counter = 1
-  temp = 0
+  max_value = -1
   while count<array.length do
     while counter<=array.length do
       if array[counter]>array[count]
